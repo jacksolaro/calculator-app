c7a0a69cc034fe516199bca1fcf5500dece55389
jacksolaro <jacksolaro@gmail.com>
2024-02-19 08:48:38 -0800

    adding back return in App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
+import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  <div className="calculator-grid">
-    <p>is this showing up now</p>
-    <div className="output">
-      <div className="previous-operand">
-        <div className="current-operand"></div>
+  return (
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="previous-operand">
+          <div className="current-operand"></div>
+        </div>
       </div>
+      <button className="span-two">AC</button>
+      <button className="">DEL</button>
+      <button className="">Div</button>
+      <button className="">1</button>
+      <button className="">2</button>
+      <button className="">3</button>
+      <button className="">4</button>
+      <button className="">5</button>
+      <button className="">6</button>
+      <button className="">7</button>
+      <button className="">8</button>
+      <button className="">9</button>
+      <button className="">-</button>
+      <button className="">.</button>
+      <button className="">0</button>
+      <button className="span-two">=</button>
     </div>
-    <button className="span-two">AC</button>
-    <button className="">DEL</button>
-    <button className="">Div</button>
-    <button className="">1</button>
-    <button className="">2</button>
-    <button className="">3</button>
-    <button className="">4</button>
-    <button className="">5</button>
-    <button className="">6</button>
-    <button className="">7</button>
-    <button className="">8</button>
-    <button className="">9</button>
-    <button className="">-</button>
-    <button className="">.</button>
-    <button className="">0</button>
-    <button className="span-two">=</button>
-  </div>;
+  );
 }
 
 export default App;
