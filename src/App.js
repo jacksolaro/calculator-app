a98d82c5434476cb88fc66577f9ee22fe81c3f3e
jacksolaro <jacksolaro@gmail.com>
2024-02-20 18:33:53 -0800

    adding clear functionality to AC button

--- src/App.js
+++ src/App.js
@@ -24,6 +24,8 @@ function reducer(state, { type, payload }) {
         ...state,
         currentOperand: `${state.currentOperand || ""}${payload.digit}`,
       };
+    case ACTIONS.CLEAR:
+      return {};
   }
 }
 
@@ -41,7 +43,12 @@ function App() {
         </div>
         <div className="current-operand">{currentOperand}</div>
       </div>
-      <button className="span-two">AC</button>
+      <button
+        className="span-two"
+        onClick={() => dispatch({ type: ACTIONS.CLEAR })}
+      >
+        AC
+      </button>
       <button className="">DEL</button>
       <OperationButton operation="÷" dispatch={dispatch} />
       <DigitButton digit="1" dispatch={dispatch} />
